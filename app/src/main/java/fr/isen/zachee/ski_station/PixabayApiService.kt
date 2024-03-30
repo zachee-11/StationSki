fa5268a78c506e8791049f55e6e58acd397bbe37
Yessine13 <yessine13@outlook.fr>
2024-03-30 15:25:23 +0100

    30/03

--- app/src/main/java/fr/isen/zachee/ski_station/PixabayApiService.kt
+++ app/src/main/java/fr/isen/zachee/ski_station/PixabayApiService.kt
@@ -1,4 +1,58 @@
 package fr.isen.zachee.ski_station
 
-class PixabayApiService {
+
+import android.content.Context
+import android.util.Log
+import com.android.volley.Request
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
+import java.net.URLEncoder
+
+class PixabayApiService(private val context: Context) {
+    companion object {
+        const val API_KEY = "42765549-14bb25b99e1c4484cd8caa190"
+        const val BASE_URL = "https://pixabay.com/api/"
+    }
+    private val queue = Volley.newRequestQueue(context)
+
+    fun fetchImages(query: String = "skieur", imageType: String = "all", callback: (List<String>) -> Unit) {
+        val url = "$BASE_URL?key=$API_KEY&q=$query&image_type=$imageType"
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            { response ->
+                val jsonObject = JSONObject(response)
+                val jsonArray = jsonObject.getJSONArray("hits")
+                val imageUrls = mutableListOf<String>()
+                for (i in 0 until jsonArray.length()) {
+                    val hit = jsonArray.getJSONObject(i)
+                    imageUrls.add(hit.getString("webformatURL"))
+                }
+                callback(imageUrls)
+            },
+            { error -> error.printStackTrace() }
+        )
+        queue.add(stringRequest)
+    }
+
+
+    fun fetchVideos(query: String = "skieur", callback: (List<String>) -> Unit) {
+        val url = "$BASE_URL/videos/?key=$API_KEY&q=$query"
+        val stringRequest = StringRequest(
+            Request.Method.GET, url,
+            { response ->
+                val jsonObject = JSONObject(response)
+                val jsonArray = jsonObject.getJSONArray("hits")
+                val videoUrls = mutableListOf<String>()
+                for (i in 0 until jsonArray.length()) {
+                    val hit = jsonArray.getJSONObject(i)
+                    videoUrls.add(hit.getJSONObject("videos").getJSONObject("large").getString("url"))
+                }
+                callback(videoUrls)
+            },
+            { error -> error.printStackTrace() }
+        )
+        queue.add(stringRequest)
+    }
+
 }
